211f04139eecdca2f36969887a2236658bcacdd7
Hanh <Hanh@PNVL70.pn.com.vn>
2022-07-19 10:11:00 +0700

    Hanh update code about us

--- src/Pages/AboutUs/indexAbout.js
+++ src/Pages/AboutUs/indexAbout.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Welcome_to from "./Welcome_to";
-import Our_doctor from "./Our_doctor";
-import Top_about from "./Top_about";
+// import Welcome_to from "./Welcome_to";
+// import Our_doctor from "./Our_doctor";
+// import Top_about from "./Top_about";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import Banner from "../../Components/Banner/Banner";
